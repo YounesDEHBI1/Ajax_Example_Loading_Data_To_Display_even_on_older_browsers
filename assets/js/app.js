d7b8f68510f061e52d007ebe7885f0b6352c574d
YounesDEHBI1 <younesdehbi.n1@gmail.com>
2020-11-18 15:09:17 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// Load new data to display
+function loadDoc() {
+    // Create an XMLHttpRequest Object for any browser
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+                this.responseText;
+        }
+    };
+    xhttp.open("GET", "assets/files/ajax_info.txt", true);
+    xhttp.send();
+}
